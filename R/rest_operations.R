dfea52f073673594ae20104f712e94abad660b46
rastoliska25 <rasto.liska25@gmail.com>
2022-10-01 18:31:23 +0200

    write data to db as timestamps and read all data

--- R/rest_operations.R
+++ R/rest_operations.R
@@ -1,31 +1,53 @@
 
+
 #' Main Operation of POST storing yearly unemployment data REST service
 #'
 #' @return
 #' @export
 #'
 #' @examples
-rest_store_yearly_unemployment_data <- function(){
-
+rest_store_yearly_unemployment_data <- function() {
   # reading data, use toyAPI::read_quandl_unemployement_data()
   read_quandl_unemployement_data()
 
   # TODO averaging by year
   res$Date <- as.Date(res$Date)   #convert to date
-  res$Year <- format(res$Date,format="%Y")    #years
+  res$Year <-
+    format(res$Date, format = "%Y-01-01 01:01:01")    #years
 
-  aggregate(res[, 2], list(res$Year), mean)   #average values by year
+  yearAverages <-
+    aggregate(res[, 2], list(res$Year), mean)  #average values by year
 
+  colnames(yearAverages)[1] <- "Date"   #change first column name
+
+  yearAverages[['Date']] <- as.POSIXct(yearAverages[['Date']],  #format to date
+                                       format = "%Y-%m-%d %H:%M:%S")
 
   # TODO store data in DB, where Date is primary key and Value numeric
+  library(DBI)
+  con <-
+    dbConnect(
+      odbc::odbc(),
+      .connection_string = "Driver={MySQL ODBC 8.0 Unicode Driver};",
+      Server = "localhost",
+      Database = "shop",
+      UID = "root",
+      PWD = "password",
+      Port = 3306
+    )
+
+  # create table and insert data
+  dbWriteTable(conn = con,
+               name = "toy_tables",
+               value = yearAverages,
+               overwrite=FALSE,
+               append=TRUE)  ## x is any data frame
 
   # TODO return calculated data as response
-
-
-
-
+  dbReadTable(con, "toy_tables")
 }
 
+rest_store_yearly_unemployment_data()
 
 
 #' Main Operation of GET yearly unemployment data REST service
@@ -37,8 +59,7 @@ rest_store_yearly_unemployment_data <- function(){
 #' @export
 #'
 #' @examples
-rest_get_yearly_unemployment_data <- function(from, to){
-
+rest_get_yearly_unemployment_data <- function(from, to) {
   # TODO reading data from database based on criteria of from/to
 
 }
