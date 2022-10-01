74170b3bf7a35f9ee08f0162b8547f9745ddf449
rastoliska25 <rasto.liska25@gmail.com>
2022-10-01 14:41:37 +0200

    api key from nasdaq added

--- R/read_quandl_unemployement_data.R
+++ R/read_quandl_unemployement_data.R
@@ -8,12 +8,14 @@
 #'
 #'apikey: ooxzxpTkpnGzBM6j9KWx
 #'
-library(magrittr)
 read_quandl_unemployement_data <- function(){
+
+  library(magrittr)
+
   logging::loginfo("Start reading Quandl data.")
 
   tryCatchLog::tryCatchLog({
-    res <- Quandl::Quandl("FRED/NROUST") %>% data.table::as.data.table()
+    res <- Quandl::Quandl("FRED/NROUST", api_key='ooxzxpTkpnGzBM6j9KWx') %>% data.table::as.data.table()
 
     res[, Date := lubridate::ymd(Date)]
   },
